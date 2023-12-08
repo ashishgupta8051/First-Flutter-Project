57823ab428a74af1077a449424c116b605db9ba5
AshishGupta <ashish.gupta@sign3labs.com>
2023-12-08 17:56:31 +0530

    create a bizcard app

--- lib/ui/bizcard.dart
+++ lib/ui/bizcard.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class BizCard extends StatelessWidget {
   const BizCard({super.key});
@@ -9,11 +11,119 @@ class BizCard extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.yellow,
         title: const Text("Biz Card"),
-
       ),
       body: Container(
+        alignment: Alignment.center,
         color: Colors.white,
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: [_getCard(), _getAvatar()],
+        ),
       ),
     );
+  }
+
+  Container _getCard() {
+    return Container(
+      height: 200,
+      width: 350,
+      margin: const EdgeInsets.all(50),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade300,
+              offset: const Offset(
+                0.0,
+                0.0,
+              ),
+              blurRadius: 14.0,
+              spreadRadius: 2.0,
+            ),
+          ]),
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Ashish Gupta",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Email: "),
+              Text("ashishgupta8051@gmail.com"),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.phone),
+              Text("6265257963"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _getAvatar() {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.all(Radius.circular(50)),
+          border: Border.all(color: Colors.red, width: 1.2),
+          image: const DecorationImage(
+              image: NetworkImage("https://picsum.photos/300/300"),
+              fit: BoxFit.cover)),
+    );
+  }
+}
+
+class MyClickableText extends StatelessWidget {
+  final String url;
+
+  const MyClickableText(this.url, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        text: '',
+        style: const TextStyle(color: Colors.black),
+        children: [
+          TextSpan(
+            text: url,
+            style: const TextStyle(
+              color: Colors.blue,
+              decoration: TextDecoration.underline,
+            ),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                launch(url);
+              },
+          ),
+        ],
+      ),
+    );
+  }
+
+  launch(String url2) async {
+    final Uri url2 = Uri.parse(url);
+    await launchUrl(url2);
+    if (!await launchUrl(url2)) {
+      // throw Exception('Could not launch $_url2');
+    }
   }
 }
